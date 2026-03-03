0cf8fba87903f357babb1dcc72c0702fda182421
Danny0327 <juliethcamposbustos@gmail.com>
2026-03-03 16:34:39 -0500

    Nav: Configuración de rutas y menú inferior funcional

--- accesorios-dm-frontend-web/src/app/app.routes.ts
+++ accesorios-dm-frontend-web/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { TiendaComponent } from './pages/tienda/tienda.component';
+import { CartComponent } from './pages/cart/cart.component'; // Importa el componente
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'inicio', component: HomeComponent },
+  { path: 'tienda', component: TiendaComponent },
+  { path: 'cart', component: CartComponent }, // <--- Esta es la ruta del carrito
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' }
+];
