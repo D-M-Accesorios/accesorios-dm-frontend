7eeeba7b527856d28cc7e01f17dcc19ee738ee84
dayana2100 <dayana.motta.camayo@gmail.com>
2026-05-11 15:58:38 -0500

    HU-DEV-DMC-FE-02-agregar-vista-catalogo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,8 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { TiendaComponent } from './pages/tienda/tienda.component';
-import { CartComponent } from './pages/cart/cart.component';
-// 1. AGREGA ESTA IMPORTACIÓN
-import { RegisterComponent } from './pages/register/register.component';
+import { CatalogComponent } from './pages/catalog/catalog.component';
 
 export const routes: Routes = [
-  { path: 'inicio', component: HomeComponent },
-  { path: 'tienda', component: TiendaComponent },
-  { path: 'cart', component: CartComponent },
-  // 2. AGREGA ESTA RUTA PARA EL BOTÓN
-  { path: 'register', component: RegisterComponent },
-  { path: '', redirectTo: 'inicio', pathMatch: 'full' }
+  { path: '', component: CatalogComponent },
+  { path: 'catalogo', component: CatalogComponent },
+  { path: '**', redirectTo: '' }
 ];
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
 import { environment } from '../../environments/environment';
-import { Product } from './product.service';
+import { Product } from '../models/product.model';
 
 export interface CartItem {
   id: number;             // id del detalle del carrito
