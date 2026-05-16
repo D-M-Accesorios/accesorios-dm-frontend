0287ad343bb30ae2bb6b1fe4cdfed872489a78d5
dayana2100 <dayana.motta.camayo@gmail.com>
2026-05-15 22:38:31 -0500

    HU-DEV-DMC-FE-03-frontend-detalle-producto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { CatalogComponent } from './pages/catalog/catalog.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 
 export const routes: Routes = [
   { path: '', component: CatalogComponent },
   { path: 'catalogo', component: CatalogComponent },
+  { path: 'producto/:id', component: ProductDetailComponent },
   { path: '**', redirectTo: '' }
 ];
