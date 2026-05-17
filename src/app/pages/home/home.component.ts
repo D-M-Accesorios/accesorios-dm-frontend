77f9bf70869e91098a327cbadf2c6f7851504639
dayana2100 <dayana.motta.camayo@gmail.com>
2026-05-16 20:39:25 -0500

    HU-DEV-DMC-FE-06-frontend-home-ecommerce-ui

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,36 +1,38 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { Product } from '../../models/product.model';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-  loginForm = new FormGroup({
-    correo: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required])
-  });
+export class HomeComponent implements OnInit {
+  products: Product[] = [];
 
-  constructor(private router: Router) {}
+  categories = [
+  { name: 'Todo', path: '/catalogo' },
+  { name: 'Collares', path: '/catalogo' },
+  { name: 'Aretes', path: '/catalogo' },
+  { name: 'Earcuff', path: '/catalogo' },
+  { name: 'Pulseras', path: '/catalogo' },
+  { name: 'Anillos', path: '/catalogo' },
+  { name: 'Sets', path: '/catalogo' },
+  { name: 'Best seller', path: '/catalogo' }
+];
 
-  onLogin() {
-    console.log("Intentando iniciar sesión...");
-    if (this.loginForm.valid) {
-      const { correo, password } = this.loginForm.value;
+  constructor(private readonly productService: ProductService) {}
 
-      // Simulación de validación
-      if (correo === 'admin@dm.com' && password === '123456') {
-        this.router.navigate(['/tienda']); // Te lleva a la imagen de accesorios
-      } else {
-        alert("Error: El usuario no existe o los datos son incorrectos.");
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe({
+      next: (products) => {
+        this.products = products.slice(0, 4);
       }
-    } else {
-      alert("Por favor, llena los campos correctamente.");
-    }
+    });
   }
 }
